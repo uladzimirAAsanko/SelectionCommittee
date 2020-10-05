7ec63a1d5612aa3fe6338121220505522f660b44
uladzimir <volodyasn@gmail.com>
2020-10-05 09:56:37 +0300

    added controller

---
+++ src/main/java/by/sanko/selectioncommittee/controller/CommandType.java
@@ -0,0 +1,9 @@
+package by.sanko.selectioncommittee.controller;
+
+public enum  CommandType {
+    WRONG_COMMAND();
+    private Command command;
+    public Command getCommand() {
+        return command;
+    }
+}
---
+++ src/main/java/by/sanko/selectioncommittee/controller/Controller.java
@@ -0,0 +1,36 @@
+package by.sanko.selectioncommittee.controller;
+
+import by.sanko.selectioncommittee.dao.pool.ConnectionPool;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(urlPatterns = "/controller")
+public class Controller extends HttpServlet {
+    private static final Logger logger = LogManager.getLogger();
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        processRequest(req,resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        processRequest(req,resp);
+    }
+
+    @Override
+    public void destroy() {
+        ConnectionPool.getINSTANCE().destroyPool();
+    }
+
+    public void processRequest(HttpServletRequest req, HttpServletResponse resp){
+
+    }
+}
